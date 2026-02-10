88737cda13c4146dd4320e1c401a8159539a5fb1
Korno <korno9612@gmail.com>
2026-02-10 22:32:31 +0200

    exercize: switch-map

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,12 +1,13 @@
-import { fromEvent, interval, merge, NEVER, timer, tap, scan, takeUntil,skipUntil } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, timer, map, tap, scan,switchMap, takeUntil,skipUntil , shareReplay} from 'rxjs';
+import { startWith } from 'rxjs/operators'
+
 import { setCount, startButton, pauseButton } from './utilities';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
+const start$ = fromEvent(startButton, 'click').pipe(map(()=> true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(map(()=> false));
 
-const counter$ = interval(1000).pipe(
-    skipUntil(start$),
+const counter$ = merge(start$, pause$).pipe(
+    switchMap((isRunning) => (isRunning ? interval(1000) : NEVER)),
     scan((total) => total + 1, 0),
-    tap(value => console.log('Counter value:', value)),
-    takeUntil(pause$));
+)
 counter$.subscribe(setCount);
