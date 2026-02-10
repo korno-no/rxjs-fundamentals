40981c1d284fc9dda898e051c799886f31f460fb
Korno <korno9612@gmail.com>
2026-02-10 16:03:44 +0200

    exersize: combining-operators

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,12 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, timer, tap, scan, takeUntil,skipUntil } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+const counter$ = interval(1000).pipe(
+    skipUntil(start$),
+    scan((total) => total + 1, 0),
+    tap(value => console.log('Counter value:', value)),
+    takeUntil(pause$));
+counter$.subscribe(setCount);
